a8893958495121dce53fe9864fb00bc0259698b8
Carlos Alvarado <Carlosalvaradoarguello10@gmail.com>
2020-10-20 16:47:44 -0500

    added endpoint to successfully save an order to our database correctly, endpoint: /purchase/save-order

--- api/purchase/PurchaseTestRouter.js
+++ api/purchase/PurchaseTestRouter.js
@@ -2,16 +2,41 @@
 const express = require('express');
 // const authRequired = require('../middleware/authRequired');
 const router = express.Router();
+const Orders = require('./PurchaseTestModel');
 const fetch = require('node-fetch');
+
+router.get('/', (req, res) => {
+  Orders.findAll()
+  .then((orders) => {
+    res.status(200).json({ orders: orders });
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json({ message: err.message }); 
+  }); 
+});
+
+// making a POST request to store order to our database
+
+router.post('/save-order', (req, res) => {
+  Orders.createOrder(req.body)
+  .then((order) => {
+    res.status(201).json({order: order }); 
+  })
+  .catch(err => {
+    console.log(err)
+    res.status(401).json({Error: err.message, message: "There was an error saving to the Database" }); 
+  })
+})
 
 // making a POST request to see if we can get a verification of price for desired quantity of soap.
 
 router.post('/', (req, res) => {
   const {
-    organizationName,
-    contactName,
-    barsRequested,
-    contactEmailAddress,
+    organization_name,
+    contact_name,
+    bars_requested,
+    contact_email,
     country,
     beneficiaries,
   } = req.body;
@@ -25,10 +50,10 @@ router.post('/', (req, res) => {
     {
     checkIfPrice(
     input: {
-    organizationName: "${organizationName}", 
-    contactName: "${contactName}",
-    barsRequested:${barsRequested},
-    contactEmailAddress: "${contactEmailAddress}",
+    organizationName: "${organization_name}", 
+    contactName: "${contact_name}",
+    barsRequested:${bars_requested},
+    contactEmailAddress: "${contact_email}",
     country: "${country}",
     beneficiaries: ${beneficiaries}
     }
@@ -51,8 +76,12 @@ router.post('/', (req, res) => {
         // console.log('TRUEEEEEEEEEEEE')
         res.status(200).json({
           data: data,
-          message: `Your Order for quantity of ${barsRequested} bars of soap will be a total of $ ${data.data.checkIfPrice.price}`,
+          message: `Your Order for quantity of ${bars_requested} bars of soap will be a total of $ ${data.data.checkIfPrice.price}`,
         });
+        // storing order in our orders table does not work with nested responses
+        // Orders.createOrder(req.body).then((order) => {
+        //   res.status(201).json({order: order }); 
+        // }); 
       }
     })
     .catch((err) => {
