82b8c8bb45ffc5ba42a7f22a6b18b6982d9b574c
tyler broadway <tbroadway96@gmail.com>
2020-10-21 16:53:35 -0500

    Updated migration & seed files w/ new fields

--- data/migrations/20200625220949_create-profile.js
+++ data/migrations/20200625220949_create-profile.js
@@ -12,12 +12,17 @@ exports.up = (knex) => {
       orders.increments('id').primary();
       orders.string('buyer_id').unsigned().references('id').inTable('profiles');
       orders.string('organization_name').notNullable();
+      orders.string('organization_website');
       orders.string('contact_name').notNullable();
-      orders.string('contact_email').notNullable();
-      orders.string('country').notNullable();
       orders.integer('bars_requested').notNullable();
+      orders.string('contact_phone').notNullable();
+      orders.string('contact_email').notNullable();
+      orders.string('location');
+      orders.string('country').notNullable();
       orders.integer('beneficiaries').notNullable();
-      orders.string('hygiene_description').notNullable();
+      orders.string('hygiene_description');
+      orders.string('hygiene_initiative').notNullable();
+      orders.string('comments');
     });
 };
 
